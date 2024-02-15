b1dfab22e55dc67e6d0e5d10ee11efd0c0b95159
Ariyan Rahman Anas <mohammadariyanrahmananas@gmail.com>
2024-02-16 02:29:31 +0600

    make the code readable in about page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -23,10 +23,12 @@ import myResume from "./../../assets/Ariyan-Rahman-Anas-Resume.pdf";
 const About = () => {
   return (
     <div>
+      {/* top section starts from here */}
       <section
         id="media-and-top-info"
         className="grid mt-16 grid-cols-1 lg:grid-cols-3 gap-8"
       >
+        {/* Ariyan Rahman Anas's cover img */}
         <div
           data-aos="zoom-in-up"
           data-aos-easing="ease-out-cubic"
@@ -36,6 +38,8 @@ const About = () => {
         >
           <img src={cover} alt="" className="rounded-lg" />
         </div>
+
+        {/* Ariyan Rahman Anas's basic info */}
         <div
           data-aos="zoom-in-up"
           data-aos-easing="ease-out-cubic"
@@ -56,14 +60,67 @@ const About = () => {
               Back-End Development. I have a diverse range of experience having
               worked across various fields and industries.
             </p>
+
+            {/* social media and resume btn */}
+            <div className="md:hidden mt-12 flex flex-col md:flex-row items-center justify-between gap-5 ">
+              <div
+                className=" social-icons w-full flex items-center justify-center gap-10 text-xl p-4 text-gray-100 rounded-lg  bg-gradient-to-l from-teal-950 to-gray-900 hover:to-gray-950 duration-500"
+                style={{
+                  boxShadow: "1px 1px 3px #042f2e,-1px -1px 3px #042f2e ",
+                }}
+              >
+                <a
+                  href="https://www.facebook.com/ariyanrahmananas69/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <p className="p-3 rounded-full bg-gradient-to-r from-teal-900 to-gray-950 hover:to-teal-600 hover:transform hover:scale-110 duration-500  ">
+                    <FaFacebookF></FaFacebookF>
+                  </p>
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/ariyanrahmananas/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <p className="p-3 rounded-full bg-gradient-to-r from-teal-900 to-gray-950 hover:to-teal-600 hover:transform hover:scale-110 duration-500">
+                    <FaLinkedinIn></FaLinkedinIn>
+                  </p>
+                </a>
+                <a
+                  href="https://github.com/Ariyan-Rahman-Anas"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <p className="p-3 rounded-full bg-gradient-to-r from-teal-900 to-gray-950 hover:to-teal-600 hover:transform hover:scale-110 duration-500">
+                    <LuGithub></LuGithub>
+                  </p>
+                </a>
+              </div>
+
+              <div
+                className="resume-download w-full flex items-center justify-center gap-10 text-xl p-4 text-gray-100 rounded-lg  bg-gradient-to-l from-teal-950 to-gray-900 hover:to-gray-950 duration-500"
+                style={{
+                  boxShadow: "1px 1px 3px #042f2e,-1px -1px 3px #042f2e ",
+                }}
+              >
+                <a
+                  href={myResume}
+                  download
+                  className="flex text-base items-center justify-between gap-2 py-2 px-4  bg-gradient-to-r from-teal-950 to-gray-950 hover:to-teal-600  rounded-md   hover:transform hover:scale-105 duration-500 "
+                >
+                  Download My Resume
+                  <LuFileDown></LuFileDown>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </section>
+      {/* top section ends here */}
 
-      <section
-        id="personal-and-educational"
-        className="my-10 grid grid-cols-1 lg:grid-cols-2 gap-8 text-gray-500"
-      >
+      {/* personal details section is starts from here */}
+      <section className="my-10 grid grid-cols-1 lg:grid-cols-2 gap-8 text-gray-500">
         <div
           data-aos="zoom-in-up"
           data-aos-easing="ease-out-cubic"
@@ -71,52 +128,59 @@ const About = () => {
           data-aos-anchor-placement="center-bottom"
           className="left text-xs 2xl:text-base px-2 py-5 md:p-5 bg-gradient-to-r from-teal-950 to-gray-950 hover:to-gray-900 duration-500 rounded-lg"
         >
-          <h1 className="text-3xl font-semibold mb-6  ">
-            Personal Information
-          </h1>
-          <div className="flex flex-col md:flex-row items-start justify-between gap-2 rounded-lg p-4 bg-gradient-to-l from-teal-950 hover:bg-gradient-to-r hover:from-teal-950 to-gray-900 hover:to-gray-950 duration-500">
-            <div className="left flex gap-1 ">
-              <div className="title flex flex-col gap-2 ">
-                <p>First Name</p>
-                <p>Age</p>
-                <p>Phone</p>
-                <p>Skype</p>
-                <p>Address</p>
+          {/* personal information */}
+          <div>
+            <h1 className="text-3xl font-semibold mb-6  ">
+              Personal Information
+            </h1>
+            <div className="flex flex-col md:flex-row items-start justify-between gap-2 rounded-lg p-4 bg-gradient-to-l from-teal-950 hover:bg-gradient-to-r hover:from-teal-950 to-gray-900 hover:to-gray-950 duration-500">
+              <div className="left flex gap-1 ">
+                <div className="title flex flex-col gap-2 ">
+                  <p>First Name</p>
+                  <p>Age</p>
+                  <p>Phone</p>
+                  <p>Skype</p>
+                  <p>Address</p>
+                </div>
+                <div className="value flex flex-col gap-2 ">
+                  <p className="font-semibold text-gray-100 ">
+                    : Ariyan Rahman
+                  </p>
+                  <p className="font-semibold text-gray-100 ">: 23 years</p>
+                  <p className="font-semibold text-gray-100 ">
+                    : +88 01610-195968
+                  </p>
+                  <p className="font-semibold text-gray-100 ">: mr.anas</p>
+                  <p className="font-semibold text-gray-100 ">
+                    : Chattogram, Bangladesh
+                  </p>
+                </div>
               </div>
-              <div className="value flex flex-col gap-2 ">
-                <p className="font-semibold text-gray-100 ">: Ariyan Rahman</p>
-                <p className="font-semibold text-gray-100 ">: 23 years</p>
-                <p className="font-semibold text-gray-100 ">
-                  : +88 01610-195968
-                </p>
-                <p className="font-semibold text-gray-100 ">: mr.anas</p>
-                <p className="font-semibold text-gray-100 ">
-                  : Chattogram, Bangladesh
-                </p>
-              </div>
-            </div>
-            <div className="right flex gap-1">
-              <div className="title flex flex-col gap-2 ">
-                <p>Last Name</p>
-                <p>Freelance</p>
-                <p>Email</p>
-                <p>Languages</p>
-                <p>Nationality</p>
-              </div>
-              <div className="value flex flex-col gap-2 ">
-                <p className="font-semibold text-gray-100 ">: Anas</p>
-                <p className="font-semibold text-gray-100 ">: Available</p>
-                <p className="font-semibold text-gray-100 ">
-                  : mohammadariyanrahmananas@gmail.com
-                </p>
-                <p className="font-semibold text-gray-100 ">
-                  : Bengali, English, Urdu and Hindi
-                </p>
-                <p className="font-semibold text-gray-100 ">: Bangladeshi</p>
+              <div className="right flex gap-1">
+                <div className="title flex flex-col gap-2 ">
+                  <p>Last Name</p>
+                  <p>Freelance</p>
+                  <p>Email</p>
+                  <p>Languages</p>
+                  <p>Nationality</p>
+                </div>
+                <div className="value flex flex-col gap-2 ">
+                  <p className="font-semibold text-gray-100 ">: Anas</p>
+                  <p className="font-semibold text-gray-100 ">: Available</p>
+                  <p className="font-semibold text-gray-100 ">
+                    : mohammadariyanrahmananas@gmail.com
+                  </p>
+                  <p className="font-semibold text-gray-100 ">
+                    : Bengali, English, Urdu and Hindi
+                  </p>
+                  <p className="font-semibold text-gray-100 ">: Bangladeshi</p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="social-icons-resume-download mt-12 flex flex-col md:flex-row items-center justify-between ">
+
+          {/* Ariyan Rahman Anas's basic info again here for medium to large devices */}
+          <div className="social-icons-resume-download hidden mt-0 md:mt-12 mb-12 md:flex flex-col md:flex-row items-center justify-between gap-5 ">
             <div
               className=" social-icons flex items-center justify-center gap-10 text-xl p-4 text-gray-100 rounded-lg  bg-gradient-to-l from-teal-950 to-gray-900 hover:to-gray-950 duration-500"
               style={{
@@ -170,6 +234,7 @@ const About = () => {
           </div>
         </div>
 
+        {/* educational information */}
         <div
           data-aos="zoom-in-up"
           data-aos-easing="ease-out-cubic"
@@ -205,9 +270,7 @@ const About = () => {
                     <p className="font-semibold text-gray-100">
                       : 4 Years, 8 Semester
                     </p>
-                    <p className="font-semibold text-gray-100">
-                      : 2023
-                    </p>
+                    <p className="font-semibold text-gray-100">: 2023</p>
                   </div>
                 </div>
               </div>
@@ -240,7 +303,9 @@ const About = () => {
           </div>
         </div>
       </section>
+      {/* personal details section is ends here */}
 
+      {/* skills section is starts from here */}
       <section
         id="skills"
         data-aos="zoom-in-up"
@@ -396,8 +461,8 @@ const About = () => {
           </div>
         </div>
       </section>
+      {/* skills section is ends here */}
     </div>
   );
 };
-
 export default About;
