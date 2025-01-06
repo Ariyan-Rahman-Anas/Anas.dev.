3a1b8f62b8f7a9be93d6a1f9a3aee37d611aa99d
Ariyan Rahman Anas <anas.hllw@gmail.com>
2025-01-06 20:45:10 +0600

    a big change in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,82 +1,56 @@
 import { Link, NavLink } from "react-router-dom";
-import { IoMenuOutline } from "react-icons/io5";
-import { RxCross2 } from "react-icons/rx";
-import { useState } from "react";
+import { AiOutlineHome } from "react-icons/ai";
+import { TiInfoLarge } from "react-icons/ti";
+import { GoProject } from "react-icons/go";
+import { MdOutlinePhoneInTalk } from "react-icons/md";
 
 const Navbar = () => {
   const navItems = [
-    { title: "Home", link: "" },
-    { title: "About", link: "about" },
-    { title: "Projects", link: "projects" },
-    { title: "Contact", link: "contact" },
+    { title: "Home", link: "", icon: <AiOutlineHome /> },
+    { title: "About", link: "about", icon: <TiInfoLarge /> },
+    { title: "Projects", link: "projects", icon: <GoProject /> },
+    { title: "Contact", link: "contact", icon: <MdOutlinePhoneInTalk /> },
   ];
-
-  const [menu, setMenu] = useState(false);
-
-  const handleHidingMenu = () => {
-    setMenu((prev) => !prev);
-  };
-
-  const menuClass = menu
-    ? "left-0 top-[3.8rem] py-10 md:py-0 right-0 bg-black rounded-md w-full min-h-screen md:min-h-0"
-    : "-left-[69rem]";
 
   return (
     <div className="sticky top-0 z-[1000]">
-      <nav className="flex flex-col md:flex-row md:items-center justify-between text-gray-500 bg-gray-950 py-3">
-        <div className="nav-left group flex items-center justify-between w-full md:w-fit z-50">
-          <Link to="/" className="bg-gradient-to-r from-white via-teal-500 to-teal-600 text-transparent bg-clip-text group-hover:from-primary group-hover:via-primary group-hover:to-primary duration-1000 animate-bounce " >
-            <h1 className="font-bruno text-3xl font-bold">Anas</h1>
-          </Link>
-          <div className="menu-btn md:hidden flex items-center">
-            <button
-              aria-label="Toggle Menu"
-              onClick={handleHidingMenu}
-              className="text-4xl duration-1000"
-            >
-              {menu ? (
-                <RxCross2 className="text-gray-300" />
-              ) : (
-                <IoMenuOutline className="text-primary" />
-              )}
-            </button>
-          </div>
-        </div>
+      <nav className="flex items-center justify-between text-gray-500 bg-gray-950 py-3">
+        <Link
+          to="/"
+          id="nav-left"
+          className="bg-gradient-to-r from-white via-teal-500 to-teal-600 text-transparent bg-clip-text group-hover:from-primary group-hover:via-primary group-hover:to-primary duration-1000 animate-bounce "
+        >
+          <h1 className="font-bruno text-3xl font-bold">Anas</h1>
+        </Link>
 
-        <div className="nav-center">
-          <ul
-            className={`flex flex-col md:flex-row items-center justify-center gap-x-8 gap-y-4 absolute md:static ${menuClass} duration-700 z-10`}
-          >
-            {navItems.map((item, index) => (
-              <li
-                key={index}
-                onClick={handleHidingMenu}
-                className="text-white relative group"
+        <ul
+          id="nav-middle"
+          className={`bg-gray-800 border md:border-none md:bg-transparent rounded-full w-[95%] mx-auto md:w-fit flex items-center justify-around md:justify-center py-2 gap-x-8 fixed bottom-[0.08rem] left-0 right-0 md:static duration-700 z-10`}
+        >
+          {navItems.map(({ title, icon, link }, index) => (
+            <li key={index} className="text-black md:text-white group">
+              <NavLink
+                to={`/${link}`}
+                className={({ isActive }) =>
+                  isActive
+                    ? "border-b-2 border-b-primary md:border-b-white rounded-sm text-white font-medium duration-500 w-fit flex items-center gap-1 relative "
+                    : "border-b-2 border-b-transparent text-gray-200 duration-500 flex items-center gap-1 relative "
+                }
               >
-                <NavLink
-                  to={`/${item.link}`}
-                  className={({ isActive }) =>
-                    isActive
-                      ? "border-b-2 border-b-white rounded-sm text-white font-medium duration-500"
-                      : "border-b-2 border-b-transparent text-gray-200 duration-500"
-                  }
-                >
-                  {item.title}
-                  <span className="absolute left-0 right-0 bottom-0 top-[1.35rem] h-[.14rem] w-full rounded-md bg-white transform scale-x-0 origin-bottom transition-transform group-hover:scale-x-100 duration-300"></span>
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-        </div>
+                <span className="text-primary md:text-white" >{icon}</span> <span className="hidden md:block">{title}</span>
+                <span className="absolute left-0 right-0 bottom-0 top-[1.35rem] h-[.14rem] w-full rounded-md bg-primary md:bg-white transform scale-x-0 origin-bottom transition-transform group-hover:scale-x-100 duration-300"></span>
+              </NavLink>
+            </li>
+          ))}
+        </ul>
 
-        <div className="nav-right hidden md:block">
-          <Link
-            to="/contact"
-            className="btn rounded-md text-gray-200 flex duration-500"
-          >
-            {`Let's talk`}
-          </Link>
-        </div>
+        <Link
+          id="nav-right"
+          to="/contact"
+          className="btn rounded-md text-gray-200 flex duration-500"
+        >
+          {`Let's talk`}
+        </Link>
       </nav>
     </div>
   );
